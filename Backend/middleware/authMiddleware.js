f0b8c98ae67188300292128c51b190dc7a628b7c
Michael-Andryeer <andryeerm@gmail.com>
2025-05-12 13:07:58 -0300

    feat(auth): Adicionado middleware de autenticação para proteção de rotas

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const authMiddleware = (request,response,next) => {
+  try {
+    const token = request.headers.authorization?.split('')[1];
+
+    if (!token) {
+      return response.status(401).json({ message: 'Acesso negado,Token não fornecido', sucess: false });
+    }
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    request.user = decoded;
+
+    next();
+  } catch (error) {
+    return response.status(401).json({ message: 'Token inválido', sucess: false });
+  }
+}
